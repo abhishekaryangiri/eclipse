453beecbe5f6f72803ee437bb8782111eeee2b60
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-09-15 23:56:10 +0530

    Update NestedLoop.java

--- CoreJava01_Basics/src/september/NestedLoop.java
+++ CoreJava01_Basics/src/september/NestedLoop.java
@@ -3,6 +3,7 @@ package september;
 public class NestedLoop {
 	
 	public static void main(String[] args) {
+		
 		for( int i=1; i<=5;) {
 			for(int j=10; j>=5;) {
 				System.out.println(i+ "  "+ j);
